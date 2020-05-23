ffc5a40f6edf77eef78ec59bf0f93e7f8e0aeaff
nachiketbhujbal <nachiketbhujbal@outlook.com>
2020-05-23 18:03:51 -0400

    update background essay

--- py/omscsapp.py
+++ py/omscsapp.py
@@ -11,42 +11,46 @@ def background_and_goals():
     """
     I graduated with a Bachelor of Science in Engineering Physics in 2018 from
     the University of Illinois at Urbana-Champaign. My specialization was in
-    Computational Physics, with a minor in Computer Science. My cumulative GPA
-    by graduation was 3.01/4.0.
+    Computational Physics, with a minor in Computer Science. My undergraduate
+    GPA was 3.01/4.0.
 
     During my undergraduate career, I worked as a data analyst on a two-man
     team in a soybean genetics lab on campus. I created a program for analyzing
-    gene sequences taken from real soybene samples and pattern-matched them
+    gene sequences taken from real soybean samples and pattern-matched them
     against a university database. I also wrote a GUI-based application wrapper
     around this program for my PI and the other RAs in our lab to use easily.
 
-    After graduation, I accepted a 1-year internship at RBC Capital Markets,
-    where I worked full time as a quant developer on the central risk trading
-    desk. I wrote software to analyze enormous sets of financial markets data
-    and built a full-stack trading platform that is now used across the company
-    by  more than 100 traders in the US, Canada, and London.
+    After graduation, I accepted a full time 1-year internship at RBC Capital
+    Markets in NYC, where I worked as a quantitative developer on the central
+    risk trading desk. I wrote software to analyze enormous sets of financial
+    markets data, worked on a project to speed up our automated trading
+    algorithms and execute orders more effeciently, and built a full-stack
+    trading platform that is now used by more than 100 traders across the US,
+    Canada, and London.
 
-    During my internship, I worked on a project to speed up our automated
-    trading algorithms and execute orders more effeciently. By then, I was fully
-    engrossed in all aspects of computer science, from software engineering, to
-    algorithmic complexity and computational theory.
-
-    By the end of the internship, I had greatly sharpened my math and critical
-    thinking skills. I had also become proficient at Python, C++, JavaScript,
-    SQL, and various other programming languages. I learned more linear algebra,
+    By the end of the internship, I was fully engrossed in all aspects of
+    computer science, from software engineering, to algorithmic complexity and
+    computational theory. I had greatly sharpened my math and critical thinking
+    skills and also become proficient at Python, C++, JavaScript, SQL, and a
+    host of other programming languages. I learned more linear algebra,
     statistics, and differential equations in that year, than I could have ever
     absorbed from undergraduate courses, since I would frequently apply concepts
     as soon as I learned them on huge sets of stochastic market data.
 
-    I was hired full time with RBC in 2019, after that year long internship.
-    Now I work on algorithmic strategy development and am also helping to build
+    I was hired full time with RBC in 2019, after that yearlong internship. Now
+    I work on algorithmic strategy development and am also helping to build
     the developer API, which provides a framework for others within the firm to
-    offer custom cross-asset strategies for their clients.
+    offer custom cross-asset strategies for clients. My goals are to drive
+    future technological innovation within RBC, and discover new limits of
+    computation.
     """
     )
     print('background_and_goals:')
     count_words(essay)
     count_chars(essay)
+    print()
+    for x in [x.strip() for x in essay.strip().split('\n')]:
+        print(x.strip('\n'))
 
 def statement_of_purpose():
     essay = (
@@ -67,7 +71,8 @@ def count_chars(essay):
         else:
             chars += len(line)
 
-    print('Character Count: {}'.format(chars))
+    if chars > 1:
+        print('Character Count: {}'.format(chars))
 
 def count_words(essay):
     essay = essay.strip()
@@ -76,8 +81,9 @@ def count_words(essay):
     for line in lines:
         words += line
 
-    print('Word Count: {}'.format(len(words)))
-    print('Number of Unique words: {}'.format(len(set(words))))
+    if len(words) > 0:
+        print('Word Count: {}'.format(len(words)))
+        print('Number of Unique words: {}'.format(len(set(words))))
 
 def main():
     print()
